d6cfb02a32f1e35976f9a421502a345ece12c83a
Benyta <kelechibenita@yahoo.com>
2023-07-19 20:21:12 +0100

    task 6

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,4 +1,4 @@
-for i in range(1, 10):
+for i in range(1, 9):
     for j in range(i + 1, 10):
-        print("{:02d}, ".format(i * 10 + j), end="")
+        print("{:02d}, ".format(i) + "{:02d}, ".format(j), end="")
 print("89")
